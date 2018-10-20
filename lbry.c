d19468072c985c821b5dc4d1e83d4826975bf9ad
tedy5 <tedy69@gmail.com>
2018-10-20 20:13:08 +0200

    Update lbry.c

--- lbry.c
+++ lbry.c
@@ -1,59 +1,65 @@
-/**
- * Migrate to node-multi-hashing package, 
- * LBRY algorithm.
- * by trustfarm@github , May 2018
- * 
- * -----------------------------------------------------
- * refer from Lbry sph Implementation 
- * tpruvot@github July 2016
- */
-
-#include "lbry.h"
-
-#include <string.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdio.h>
 
-#include "sha3/sph_sha2.h"
+#include "sha3/sph_sha2_lbry.h"
 #include "sha3/sph_ripemd.h"
 
-void lbry_hash(const char* input, char* output)
+//#define DEBUG
+
+#ifdef DEBUG
+static void hexlify(char *hex, const unsigned char *bin, int len)
 {
-    char    hashA[64];
-    char    hashB[32];
-    char    hashC[32];
+	hex[0] = 0;
+	for(int i=0; i < len; i++)
+		sprintf(hex+strlen(hex), "%02x", bin[i]);
+}
+#endif
 
-    sph_sha256_context ctx_sha256;
-    sph_sha512_context ctx_sha512;
-    sph_ripemd160_context ctx_ripemd;
+void lbry_hash(const char* input, char* output, uint32_t len)
+{
+	uint32_t hashA[16];
+	uint32_t hashB[8];
+	uint32_t hashC[8];
 
-    // sha256d - hashA, hashA-MSB
-    sph_sha256_init(&ctx_sha256);
-    sph_sha256(&ctx_sha256, input, 112);
-    sph_sha256_close(&ctx_sha256, hashA);
-    sph_sha256(&ctx_sha256, hashA, 32);
-    sph_sha256_close(&ctx_sha256, hashA);
+	sph_sha256_context ctx_sha256;
+	sph_sha512_context ctx_sha512;
+	sph_ripemd160_context ctx_ripemd;
 
-    // sha512 - hashA
-    sph_sha512_init(&ctx_sha512);
-    sph_sha512(&ctx_sha512, hashA, 32);
-    sph_sha512_close(&ctx_sha512, hashA);
+	sph_sha256_init(&ctx_sha256);
+	sph_sha512_init(&ctx_sha512);
+	sph_ripemd160_init(&ctx_ripemd);
 
-    // ripemd160d - hashA-MSB , close hashB
-    sph_ripemd160_init(&ctx_ripemd);
-    sph_ripemd160(&ctx_ripemd, hashA, 32);
-    sph_ripemd160_close(&ctx_ripemd, hashB);
+	sph_sha256(&ctx_sha256, input, 112);
+	sph_sha256_close(&ctx_sha256, hashA);
+	sph_sha256(&ctx_sha256, hashA, 32);
+	sph_sha256_close(&ctx_sha256, hashA);
 
-    // ripemd160d - hashA-LSB , close hashC
-	sph_ripemd160(&ctx_ripemd, &hashA[32], 32); // weird
+	sph_sha512(&ctx_sha512, hashA, 32);
+	sph_sha512_close(&ctx_sha512, hashA);
+
+	sph_ripemd160(&ctx_ripemd, hashA, 32);  // sha512 low
+	sph_ripemd160_close(&ctx_ripemd, hashB);
+
+	sph_ripemd160(&ctx_ripemd, &hashA[8], 32); // sha512 high
 	sph_ripemd160_close(&ctx_ripemd, hashC);
 
-    // sha256d - hashB , hashC , close hashA
-    sph_sha256(&ctx_sha256, hashB, 20);
-    sph_sha256(&ctx_sha256, hashC, 20);
+	sph_sha256(&ctx_sha256, hashB, 20);
+	sph_sha256(&ctx_sha256, hashC, 20);
 	sph_sha256_close(&ctx_sha256, hashA);
-    // sha256d - hashA 
-    sph_sha256(&ctx_sha256, hashA, 32);
-    sph_sha256_close(&ctx_sha256, hashA);
+
+	sph_sha256(&ctx_sha256, hashA, 32);
+	sph_sha256_close(&ctx_sha256, hashA);
 
 	memcpy(output, hashA, 32);
+
+#ifdef DEBUG
+	char hex[512] = { 0 };
+	hexlify(hex, input, len);
+	fprintf(stderr, "input  %s (%d)\n", hex, len);
+
+	hexlify(hex, output, 32);
+	fprintf(stderr, "output %s\n", hex);
+#endif
 }
